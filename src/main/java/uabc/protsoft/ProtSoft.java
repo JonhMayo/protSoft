027b4dfafc07b4fa1e3ec34ea300b4c28675b7e8
JonhMayo <granados.jonathan@uabc.edu.mx>
2024-03-11 13:45:14 -0700

    Se agrega el proyecto

---
+++ src/main/java/uabc/protsoft/ProtSoft.java
@@ -0,0 +1,18 @@
+/*
+ */
+
+package uabc.protsoft;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class ProtSoft {
+    
+    private static protInter inter;
+
+    public static void main(String[] args) {
+        inter = new protInter();
+        inter.setVisible(true);
+    }
+}
